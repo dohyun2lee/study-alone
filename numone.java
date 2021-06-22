752a90480aa9951690488f191ca5b554edc0e820
woerkdose <84783014+woerkdose@users.noreply.github.com>
2021-06-22 21:59:49 +0900

    Add files via upload
    ch1,ch2

---
+++ numone.java
@@ -0,0 +1,22 @@
+
+public class numone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		int y = 4;
+		System.out.print(" x = ");
+		System.out.println(x);
+		System.out.print(" y = ");
+		System.out.println(y);
+		System.out.print(" x + y = ");
+		System.out.println(x+y);
+		System.out.print(" x - y = ");
+		System.out.println(x-y);
+		System.out.print(" x * y = ");
+		System.out.println(x*y);
+		System.out.print(" x / y = ");
+		System.out.println(x/y);		
+	}
+
+}
