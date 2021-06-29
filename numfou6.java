a4f333adc0afa27554cbed241e9a8613db07ea99
woerkdose <84783014+woerkdose@users.noreply.github.com>
2021-06-29 22:54:20 +0900

    Add files via upload
    ch8,9

---
+++ numfou6.java
@@ -0,0 +1,30 @@
+
+class Card {
+	String kind;
+	int number;
+	
+	Card() {
+		this("Spade", 1);
+	}
+
+	Card(String kind, int number) {
+		this.kind = kind;
+		this.number = number;
+	}
+	
+	public String toString() {
+		return "king :" +kind+ ", number :"+number;
+	}
+}
+
+public class numfou6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Card c1 = new Card();
+		Card c2 = new Card("Heart", 10);
+		System.out.println(c1.toString());
+		System.out.println(c2.toString("Heart", 10));
+	}
+
+}
---
+++ numone6.java
@@ -0,0 +1,11 @@
+
+/*
+ * public class numone6 {
+ * 
+ * public static void main(String[] args) { // TODO Auto-generated method stub
+ * try { Exception e = new Exception("고의로 발생"); throw e; } catch (Exception e) {
+ * System.out.println("에러 메시지 :" +e.getMessage())); e.printStackTrace(); }
+ * System.out.println("프로그램이 정상종료"); }
+ * 
+ * }
+ */
