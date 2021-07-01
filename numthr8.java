d1736b12d3ceec4c48d8c12689b1a9fb45315b60
woerkdose <84783014+woerkdose@users.noreply.github.com>
2021-07-01 23:30:52 +0900

    Add files via upload
    ch9

---
+++ numthr8.java
@@ -0,0 +1,13 @@
+
+public class numthr8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[] cArr = new char[0];  
+		String s = new String(cArr); 
+
+		System.out.println("cArr.length="+cArr.length);
+		System.out.println("@@@"+s+"@@@");
+	}
+
+}
