cd808c89eac51c445239fd60daa1566a4a75e3ed
woerkdose <84783014+woerkdose@users.noreply.github.com>
2021-07-11 00:53:33 +0900

    Add files via upload

--- numthr14.java
+++ numthr14.java
@@ -14,17 +14,32 @@ public class numthr14 {
 	}
 
 }
-
+ 
 class Person {
 	String name;
 	int age;
+	
+	public int hashCode() {
+		return Objects.hash(name, age);
+	}
+	
+	public boolean equals(Object obj) {
+		if(!(obj instanceof Person)) return false;
+		Person p = (Person)obj;
+		return this.name.equals(p.name) && this.age==p.age;
+	}
 	
 	Person(String name, int age) {
 		this.name = name;
 		this.age = age;
 	}
 	
+	
 	public String toString() {
 		return name + ":" +age;
 		}
 }
+
+
+
+
