86a0672e36b852136fd690b8cc29c89dd859e632
woerkdose <84783014+woerkdose@users.noreply.github.com>
2021-07-23 23:47:32 +0900

    Add files via upload

---
+++ numone26.java
@@ -0,0 +1,43 @@
+
+public class numone26 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Runnable r = new Runnable1();
+		new Thread(r).start();
+		new Thread(r).start();
+		
+	}
+
+}
+
+class Account {
+	private int balance = 1000;
+
+	public int getBalance() {
+		return balance;
+	}
+	
+	public void withdraw(int money) {
+		if(balance >= money) {
+			try {Thread.sleep(1000);
+			}
+			catch (InterruptedException e) {}
+			balance -= money;
+		}
+	}
+}
+
+class Runnable1 implements Runnable {
+	Account acc = new Account();
+	
+	
+	public void run() {
+		while(acc.getBalance() > 0 ) {
+			int money = (int)(Math.random() * 3 + 1) * 100;
+			acc.withdraw(money);
+			System.out.println("balance : " +acc.getBalance());
+		}
+	}
+}
+
