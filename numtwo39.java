db44eb4950929d8988da63a81589ce8397d17617
woerkdose <84783014+woerkdose@users.noreply.github.com>
2021-08-05 01:45:48 +0900

    Add files via upload

---
+++ numtwo39.java
@@ -0,0 +1,10 @@
+
+public class numtwo39 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("out : Hello World!");
+		System.err.println("err : Hello World!");
+	}
+
+}
