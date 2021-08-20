a2e6481b568dfa581dd2427d212ac3bde07a8d4f
woerkdose <84783014+woerkdose@users.noreply.github.com>
2021-08-20 23:47:06 +0900

    Add files via upload

--- stringtri.java
+++ stringtri.java
@@ -6,31 +6,29 @@ public class stringtri {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
-		int i = 0, j = 0, k = 0, n = 0;	
+		int i = 0, j = 0, k = 0, n = 0, a = 65, b = 0;	
 		n = sc.nextInt();
-		String[] arr = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", 
-				"K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-		//int [][] arr = new int[n][n];
-		//int alpha = 0;
+		char[][] arr = new char[n][n];
 		
-		//for (i = 1; i <= n; i++) {
-		   // for (j = i, k = n; j <= n; j++, k--) {
-		        //arr[j][k] = alpha++;
-		        //if(alpha > 'Z')
-		           // alpha = 'A';
-		        
-		        //System.out.print(alpha);
-		    //}
-		//}
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < n; j++) {
+				arr[i][j] = ' ';
+			}
+		}
 		
-			for(i = 0; i < n; i++) {
-			for(k = n-1, j = 0; k >= 0 && j < n; k--, j++) {
-				System.out.print("   ");
-				System.out.print(arr[j]+" ");
+		for(k = 0; k < n; k++) {
+			for(i = 0; k+i < n; i++) {
+				arr[k+i][n-1-i] = (char)(a+b%26);
+				b++;
+			}
+		}
+		
+		for(i = 0; i < n; i++) {
+			for(j = 0; j < n; j++) {
+				System.out.print(arr[i][j] + " ");
 			}
 			System.out.println();
 		}
-
 	}
 
 }
