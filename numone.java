36c378610c710ea3dbfed3bea029192424b7bfc9
woerkdose <84783014+woerkdose@users.noreply.github.com>
2021-08-21 23:53:28 +0900

    Add files via upload

--- numone.java
+++ numone.java
@@ -1,22 +1,39 @@
+import java.util.*;
 
 public class numone {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x = 10;
-		int y = 4;
-		System.out.print(" x = ");
-		System.out.println(x);
-		System.out.print(" y = ");
-		System.out.println(y);
-		System.out.print(" x + y = ");
-		System.out.println(x+y);
-		System.out.print(" x - y = ");
-		System.out.println(x-y);
-		System.out.print(" x * y = ");
-		System.out.println(x*y);
-		System.out.print(" x / y = ");
-		System.out.println(x/y);		
-	}
+		Scanner sc = new Scanner(System.in);
+		
+		int n = 0, i = 0, j = 0, a = 0, b = 0;
+		n = sc.nextInt();
+		int[] arr = new int[n];
+		arr[i] = sc.nextInt();
 
+		if(arr[i] <= 1000000) {
+				for(j=2;j<arr[i];j++) {
+					if(arr[i]%j==0&&j!=arr[i]) {
+						if(a%j!=0&&j!=a&&a<=j&&b%j!=0&&j!=b&&b<=j&&a!=b) {
+							if(Math.abs(arr[i]-a)>Math.abs(arr[i]-b)) {
+								System.out.print(b);
+							}
+							else if(Math.abs(arr[i]-a)<Math.abs(arr[i]-b)) {
+								System.out.print(a);
+							}
+							else {
+								if(a>b)
+									System.out.print(b + a);
+								else
+									System.out.print(a + b);
+							}
+						}
+					}
+					else
+						System.out.println(arr[i]+" ");
+			}
+		}
+	}
 }
+
+
