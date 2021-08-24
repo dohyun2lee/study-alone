5f2b82be48dd5a264c1fb0bb07e610203d1e61e2
woerkdose <84783014+woerkdose@users.noreply.github.com>
2021-08-24 23:40:48 +0900

    Add files via upload

--- numone.java
+++ numone.java
@@ -6,44 +6,39 @@ public class numone {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
-		int n = 0, m = 0;
+		String as = sc.next();
+		char[] arr = as.toCharArray();
+		int i = 0, j = 0;
 		
-		n = sc.nextInt();
-		m = sc.nextInt();
-		
-		if(n<=101&&1<=m&&m<=3) {
-			switch(m) {
-			case 1 :
-				for(int i=1;i<n;i++) {
-					for(int j=0;j<i;j++) {
-						System.out.print("*");
-					}
-					System.out.println();
-				}
-				break;
-			case 2 :
-				for(int i=n;i>0;i--) {
-					for(int j=i;j>0;j--) {
-						System.out.print("*");
-					}
-					System.out.println();
-				}
-				break;
-			case 3 :
-				for(int i=0;i<n;i++) {
-					for(int j=0;j<n-i;j++) {
-						System.out.print(" ");
-					}
-					for(int j=0;j<1+i*2;j++) {
-						System.out.print("*");
-					}
-					System.out.println();
-				}
-				break;
+		System.out.println("eydbkmiqugjxlvtzpnwohracsf");
+		System.out.print(arr[i]);
+//		
+//		for() {
+//			if(Character.isUpperCase(arr[i])==true) {
+//				arr[i] = (char)(j+65);
+//			}
+//			else
+//				arr[i] = (char)(j+97);
+//			System.out.println(arr[i]);
+//		}
+		for(i = 0, j = 0; i < arr.length&& j < 26; i++, j++) {
+			if(arr[i] == ' ') {
+				//System.out.print(arr[i]);
+				continue;
+			}
+			else if(65<=arr[i]&&arr[i]< 97) {
+				arr[i] = (char)(j+65); 
+				//System.out.print(arr[i]);
+				continue;
+			}
+			else {
+				arr[i] = (char)(j+97);
+				//System.out.print(arr[i]);	
+				continue;
 			}
 		}
-		else
-			System.out.println("Input Error!");
+		System.out.println(arr[i]);
+		
 	}
 
 }
