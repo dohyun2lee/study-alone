d19ba1e878e10b5156c8fa0ac8000aa2aed2768a
woerkdose <84783014+woerkdose@users.noreply.github.com>
2021-08-25 23:23:54 +0900

    Add files via upload

--- numone.java
+++ numone.java
@@ -6,14 +6,19 @@ public class numone {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
+		String bs = sc.next();
+		char[] key = bs.toCharArray();
 		String as = sc.next();
 		char[] arr = as.toCharArray();
+			
+		String[] alp = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"
+				, "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+		
 		int i = 0, j = 0;
 		
-		System.out.println("eydbkmiqugjxlvtzpnwohracsf");
-		System.out.print(arr[i]);
+		//System.out.println("eydbkmiqugjxlvtzpnwohracsf");
 //		
-//		for() {
+//		for(i = 0, j = 0; i < arr.length&& j < 26; i++, j++) {
 //			if(Character.isUpperCase(arr[i])==true) {
 //				arr[i] = (char)(j+65);
 //			}
@@ -21,24 +26,45 @@ public class numone {
 //				arr[i] = (char)(j+97);
 //			System.out.println(arr[i]);
 //		}
+		
+		
+		
+		
+//		for(i = 0, j = 0; i < arr.length&& j < 26; i++, j++) {
+//			if(arr[i] == ' ') {
+//				//System.out.print(arr[i]);
+//				continue;
+//			}
+//			else if(65<=arr[i]&&arr[i]< 97) {
+//				arr[i] = (char)(j+65); 
+//				//System.out.print(arr[i]);
+//				continue;
+//			}
+//			else {
+//				arr[i] = (char)(j+97);
+//				//System.out.print(arr[i]);	
+//				continue;
+//			}
+//		}
+//		for(i = 0; i < arr.length; i++) {
+//			System.out.print(arr[i]);
+//		}
+		
+		
+		//, "A", "B", "C", "D", "E", "F", "G", 
+		//"H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R ", "S", "T", "U" , "V", "W", "X", 
+		//"Y", "Z"
+		
 		for(i = 0, j = 0; i < arr.length&& j < 26; i++, j++) {
 			if(arr[i] == ' ') {
-				//System.out.print(arr[i]);
-				continue;
+				System.out.print(' ');
 			}
-			else if(65<=arr[i]&&arr[i]< 97) {
-				arr[i] = (char)(j+65); 
-				//System.out.print(arr[i]);
-				continue;
+			else if(65<=arr[i]&&arr[i]<97&&arr[i]==key[j]) {
+				System.out.print(alp[j].toUpperCase());
 			}
-			else {
-				arr[i] = (char)(j+97);
-				//System.out.print(arr[i]);	
-				continue;
-			}
+			else 
+				System.out.print(alp[j]);
 		}
-		System.out.println(arr[i]);
-		
 	}
 
 }
