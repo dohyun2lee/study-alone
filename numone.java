f6441cf0371961de09cac84922a63b721409c8ea
woerkdose <84783014+woerkdose@users.noreply.github.com>
2021-08-28 17:16:29 +0900

    Add files via upload

--- numone.java
+++ numone.java
@@ -8,43 +8,38 @@ public class numone {
 		
 		String bs = sc.next();
 		char[] key = bs.toCharArray();
+		//eydbkmiqugjxlvtzpnwohracsf
 		String as = sc.next();
 		char[] arr = as.toCharArray();
 		
 		String alp = "abcdefghijklmnopqrstuvwxyz";
 		char[] allp = alp.toCharArray();
-		String alp2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		char[] allp2 = alp2.toCharArray();
 		
-		int i = 0, j = 0, n = 90;
-		
-		String spa = " ";
-		char[] space = spa.toCharArray();
+		int i = 0, j = 0;
+		int n = arr.length;
 		
 		char[] ans = new char[n];
 		
-		//System.out.println("eydbkmiqugjxlvtzpnwohracsf");
-		
 		for(i = 0; i < arr.length; i++) {
-			for(j = 0; j < 26; j++) {
-				if(arr[i] == ' ') {
-					ans[i] += (char)(' '); 
+			for(j = 0; j < allp.length; j++) {
+				if(arr[i]==' ') {
+					ans[i] += (char)(32);
 					continue;
 				}
 				else if(Character.isUpperCase(arr[i])) {
-					if(allp[j]==arr[i]+32)
+					if(allp[j] == arr[i]+32) {
 						ans[i] += key[j]-32;
+					}
 					continue;
 				}
-				else 
-					if(allp[j]==arr[i])
+				else
+					if(allp[j] == arr[i]) {
 						ans[i] += key[j];
-					continue;
+					}
+				continue;
 			}
+			System.out.print(ans[i]);
 		}
-		System.out.print(ans);
-		
-	
 	}
 
 }
