a55ee9de10bfba8bfd536e96a0cb5eeac0c3a32b
이도현 <84783014+woerkdose@users.noreply.github.com>
2021-09-15 23:54:48 +0900

    Add files via upload

---
+++ GetServlet.java
@@ -0,0 +1,31 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.annotation.WebServlet;
+@WebServlet
+
+public class GetServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+
+	protected void doGet(jakarta.servlet.http.HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String msg = request.getParameter("msg");
+		response.setContentType("text/html; charset=EUC-KR");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Get Servlet ¹æ½Ä</h1>");
+		out.println("<h2>msg : "+msg +"</h2>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	
+}
