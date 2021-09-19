dc847d96269e95c4a129a683f2675cfe09614c47
이도현 <84783014+woerkdose@users.noreply.github.com>
2021-09-20 01:47:37 +0900

    Add files via upload

---
+++ insert into.sql
@@ -0,0 +1,5 @@
+INSERT INTO
+tblregister(ID, PWD, NAME, NUM1, NUM2, EMAIL, PHONE, ZIPCODE, ADDRESS, JOB)
+VALUES('rorod', '1234', '이경미', '123456', '1234567', 'rorod@naver.com', '010-1234-1234', '1234', '강남구', '개발자');
+
+SELECT * FROM tblregister;
