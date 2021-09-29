3021e8e30e476529e47493486a22c8caea382b07
이도현 <84783014+woerkdose@users.noreply.github.com>
2021-09-29 23:49:48 +0900

    Add files via upload

---
+++ ELMethod.java
@@ -0,0 +1,11 @@
+package chapter18;
+
+import java.text.DecimalFormat;
+
+public class ELMethod {
+
+	public static String comma(int number) {
+		DecimalFormat df = new DecimalFormat("#,##0");
+		return df.format(number);
+	}
+}
