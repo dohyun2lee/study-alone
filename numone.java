2e5ffc4faf3bc4c2d104d6ffa1e8f9e2f7d13ab6
이도현 <84783014+dohyun2lee@users.noreply.github.com>
2023-05-31 13:17:00 +0900

    Add files via upload

--- numone.java
+++ numone.java
@@ -1,42 +1,22 @@
-import java.util.*;
 
 public class numone {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		String bs = sc.nextLine();
-		char[] key = bs.toCharArray();
-		//eydbkmiqugjxlvtzpnwohracsf
-		String as = sc.nextLine();
-		char[] arr = as.toCharArray();
-		
-		String alp = "abcdefghijklmnopqrstuvwxyz";
-		char[] allp = alp.toCharArray();
-		
-		int i = 0, j = 0;
-		int n = arr.length;
-		
-		char[] ans = new char[n];
-		
-		for(i = 0; i < arr.length; i++) {
-			for(j = 0; j < allp.length; j++) {
-				if(arr[i]==' ') {
-					ans[i] += (char)(32);
-				}
-				else if(Character.isUpperCase(arr[i])) {
-					if(allp[j] == arr[i]+32) {
-						ans[i] += key[j]-32;
-					}
-				}
-				else
-					if(allp[j] == arr[i]) {
-						ans[i] += key[j];
-					}
-			}
-			System.out.print(ans[i]);
-		}
+		int x = 10;
+		int y = 4;
+		System.out.print(" x = ");
+		System.out.println(x);
+		System.out.print(" y = ");
+		System.out.println(y);
+		System.out.print(" x + y = ");
+		System.out.println(x+y);
+		System.out.print(" x - y = ");
+		System.out.println(x-y);
+		System.out.print(" x * y = ");
+		System.out.println(x*y);
+		System.out.print(" x / y = ");
+		System.out.println(x/y);		
 	}
 
 }
